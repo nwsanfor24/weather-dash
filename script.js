4e9dc01efd7d18991d7b1840aab4d46d025c3a19
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-06 22:17:30 -0500

    append elements on page

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 // $(document).ready(function () {
 
-    var apikey = "945dc5fe55069a270dcc159ead1201ef";
-    var city = "portsmouth";
+var apikey = "945dc5fe55069a270dcc159ead1201ef";
+var city = "portsmouth,us";
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apikey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apikey;
+var queryURL2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "APPID=" + apikey;
 
-    $.ajax({
-        //$.curl({
-        url: queryURL,
-        method: "GET"
-      }).done(function(response) {
-        
-        console.log(response);
-        
-        
-        });
-    
-    // var ourRequest = new XMLHttpRequest();
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).done(function (response) {
 
-    // ourRequest.open(
-    //     "GET",
-    //     "http://api.openweathermap.org/data/2.5/weather?q=" + 
-    //     city + 
-    //     "&APPID=" + apikey
-    // );
+  console.log(response);
 
-    // ourRequest.onload = function() {
-        
-    //     var ourData = ourRequest.responseText;
+  var name = response.name;
+  console.log(name);
+  $("#title").append(name);
 
-    //     console.log(ourData);
+  var temp = response.main.temp;
+  console.log("temperature: " + temp);
+  $("#temp").append(temp);
 
-    // }
-// });
+  var humidity = response.main.humidity;
+  console.log("humidity: " + humidity);
+  $("#humid").append(humidity);
+
+  var windSpeed = response.wind.speed;
+  console.log("wind speed: " + windSpeed);
+  $("#wind").append(windSpeed);
+
+});
+
+$.ajax({
+  url: queryURL2,
+  method: "GET"
+}).done(function (response) {
+  console.log(response);
+})
 
